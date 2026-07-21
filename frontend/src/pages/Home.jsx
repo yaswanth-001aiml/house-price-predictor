fba060ae34cddc306bd7b0a55ac2c76572865193
yaswanth-001aiml <yaswanthmanikantathota@gmail.com>
2026-07-22 00:10:06 +0530

    Add files via upload

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,1 +1,43 @@
+import { useState } from "react";
 
+import Navbar from "../components/Navbar";
+import PredictionForm from "../components/PredictionForm";
+import ResultCard from "../components/ResultCard";
+import Footer from "../components/Footer";
+
+import "../styles/Home.css";
+
+function Home() {
+
+  const [price, setPrice] = useState("");
+
+  const predictPrice = (data) => {
+
+    console.log(data);
+
+    // Temporary fake prediction
+    const prediction =
+      Number(data.area) * 5000 +
+      Number(data.bedrooms) * 200000 +
+      Number(data.bathrooms) * 100000 +
+      Number(data.parking) * 50000 -
+      Number(data.age) * 10000;
+
+    setPrice(prediction.toLocaleString());
+
+  };
+
+  return (
+    <>
+      <Navbar />
+
+      <PredictionForm onPredict={predictPrice} />
+
+      <ResultCard price={price} />
+
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
